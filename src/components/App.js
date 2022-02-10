0c814519105529f2427489cab9787cd8e2b88607
James Corbould <james.bron@hotmail.com>
2022-02-10 21:42:00 +1300

    Removed unused var.

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import Game from './Game';
 
 const StarMatch = () => {
   const [gameId, setGameId] = useState(1);
-  const unused = 'something';
   return <Game key={gameId} startNewGame={() => setGameId(gameId + 1)} />;
 };
 
