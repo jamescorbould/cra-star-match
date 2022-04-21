8991dc13e2d9d2ff12992d22e90a1f3ac4ea8bee
James Corbould <james.bron@hotmail.com>
2022-04-21 21:52:30 +1200

    Finally fixed sqlite connection string path to ensure accessed correctly but Docker mounted volume.

--- Backend/LeaderBoardApi/commands.sh
+++ Backend/LeaderBoardApi/commands.sh
@@ -1,6 +1,5 @@
 dotnet publish -c Release
 docker build -t leaderboard-api -f Dockerfile .
-docker create --name leaderboard-api leaderboard-api
-docker run -d -p 7190:8080 leaderboard-api 
-docker start leaderboard-api
+docker volume create leaderboard-db
+docker run -dp 7190:8080 -v leaderboard-db:/data leaderboard-api
 docker exec -it leaderboard-api /bin/bash
